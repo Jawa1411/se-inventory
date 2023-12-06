1b9551c2ad01801a3a0693e1e634a46b6d8d661f
Jawa1411 <jawaharsenthil1411@gmail.com>
2023-12-06 16:24:10 +0530

    Changes for Form and List view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import itemsList from '../assets/items.json';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+interface item {
+  Name: string,
+  Price: number,
+  InStock: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'se-inventory';
+  itemsList: item[] = itemsList;
+  ItemForm = this.formBuilder.group({
+    Name: '',
+    Price: '',
+    InStock: ''
+  });
+
+  constructor (
+    private formBuilder: FormBuilder
+  ) {
+
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.ItemForm.value);
+  }
 }
